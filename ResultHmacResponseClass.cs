ab083f8b615747370f669e60cc04ae54997df5b7
badhitman <10000335+badhitman@users.noreply.github.com>
2019-07-03 13:37:10 +0300

    Create ResultHmacResponseClass.cs

---
+++ ResultHmacResponseClass.cs
@@ -0,0 +1,37 @@
+﻿////////////////////////////////////////////////
+// © https://github.com/badhitman - @fakegov 
+////////////////////////////////////////////////
+using System.Runtime.Serialization;
+
+namespace HmacHttp
+{
+    public enum StatusResult
+    {
+        /// <summary>
+        /// Не установлено (не известно)
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Ошибок не обнаружено
+        /// </summary>
+        Ok,
+
+        /// <summary>
+        /// Обнаружены ошибки. Смотрите в описании статуса и логах
+        /// </summary>
+        Err
+    }
+    [DataContract]
+    public class ResultHmacResponseClass
+    {
+        [DataMember]
+        public StatusResult status = StatusResult.None;
+
+        [DataMember]
+        public string StatusDescription;
+
+        [DataMember]
+        public string Response;
+    }
+}
